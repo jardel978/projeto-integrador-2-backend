621b4f05be3493c9e74fe3135a2472d320767b6b
Nelson Kobayashi <koba.nelson@gmail.com>
2023-02-09 21:25:33 -0300

    subindo esqueleto do projeto

---
+++ ms-discovery/src/test/java/com/dmh/msdiscovery/MsDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmh.msdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
