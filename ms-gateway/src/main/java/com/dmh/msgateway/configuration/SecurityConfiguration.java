eeaf87db93f1680f0191dda98e1306367d3ed5e4
Jardel Silva <jardeloliver197@gmail.com>
2023-02-17 21:02:30 -0300

    removendo exception handler de acesso negado

--- ms-gateway/src/main/java/com/dmh/msgateway/configuration/SecurityConfiguration.java
+++ ms-gateway/src/main/java/com/dmh/msgateway/configuration/SecurityConfiguration.java
@@ -1,11 +1,8 @@
 package com.dmh.msgateway.configuration;
 
-import com.dmh.msgateway.exceptions.CustomAccessDeniedHandler;
-import com.dmh.msgateway.exceptions.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -16,7 +13,7 @@ public class SecurityConfiguration {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange()
-                .pathMatchers(HttpMethod.GET,"/actuator/**").permitAll()
+                .pathMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                 .and()
                 .authorizeExchange()
                 .pathMatchers(HttpMethod.POST, "/ms_users/users/users/registration", "/ms_users/users/users/login").permitAll()
@@ -27,10 +24,6 @@ public class SecurityConfiguration {
                 .and()
                 .oauth2Login()
                 .and().oauth2ResourceServer(cfg -> cfg.jwt())
-//                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
-//                .and()
-//                .exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-//                .and()
                 .csrf().disable()
         ;
 
