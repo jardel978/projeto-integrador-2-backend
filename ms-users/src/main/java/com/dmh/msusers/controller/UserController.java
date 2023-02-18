5fd102cd76d57791512183da4e7a7436d104de96
Jardel Silva <jardeloliver197@gmail.com>
2023-02-17 21:19:30 -0300

    keycloak logout

--- ms-users/src/main/java/com/dmh/msusers/controller/UserController.java
+++ ms-users/src/main/java/com/dmh/msusers/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.dmh.msusers.controller;
 
 import com.dmh.msusers.exceptions.InvalidFieldException;
+import com.dmh.msusers.model.dto.LoginCredentialsDTO;
 import com.dmh.msusers.model.dto.UserDTORequest;
 import com.dmh.msusers.response.ResponseHandler;
 import com.dmh.msusers.service.UserServiceImpl;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -36,20 +38,20 @@ public class UserController {
         return responseHandler.build(userService.findById(id), HttpStatus.OK, "user found.");
     }
 
-//    @PostMapping("/login")
-//    public ResponseEntity<Object> login(@Valid @RequestBody LoginCredentialsDTO loginCredentials, BindingResult result) {
-//        if (result.hasErrors())
-//            throw new InvalidFieldException(result.getAllErrors().get(0).getDefaultMessage());
-//
-//        return responseHandler.build(userService.login(loginCredentials.getEmail(), loginCredentials.getPassword()),
-//                HttpStatus.OK, "successfully logged.");
-//    }
-//
-//    @PostMapping("/logout/{id}")
-//    public ResponseEntity<Object> logout(@PathVariable("id") String id,
-//                                         HttpServletRequest request) {
-//        userService.logout(request, id);
-//        return responseHandler.build(null, HttpStatus.OK, "successfully logout.");
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@Valid @RequestBody LoginCredentialsDTO loginCredentials, BindingResult result) {
+        if (result.hasErrors())
+            throw new InvalidFieldException(result.getAllErrors().get(0).getDefaultMessage());
+
+        return responseHandler.build(userService.login(loginCredentials.getEmail(), loginCredentials.getPassword()),
+                HttpStatus.OK, "successfully logged.");
+    }
+
+    @PostMapping("/logout/{id}")
+    public ResponseEntity<Object> logout(@PathVariable("id") String id,
+                                         HttpServletRequest request) {
+        userService.logout(request, id);
+        return responseHandler.build(null, HttpStatus.OK, "successfully logout.");
+    }
 
 }
