8c2a3808c235be5ae34b3b832a0f871cf85b3850
Giovani Silva <giovannispaula@gmail.com>
2023-03-06 23:03:54 -0300

    feature[tasks-14]: Alterações na consulta /accounts/id/cads/id GET;

--- ms-accounts/src/main/java/com/dmh/msaccounts/controller/TransactionController.java
+++ ms-accounts/src/main/java/com/dmh/msaccounts/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.dmh.msaccounts.controller;
 
 import com.dmh.msaccounts.model.Transactions;
+import com.dmh.msaccounts.model.dto.TransactionDtoRequest;
 import com.dmh.msaccounts.model.enums.CardsTypeEnum;
 import com.dmh.msaccounts.response.ResponseHandler;
 import com.dmh.msaccounts.service.IAccountService;
@@ -8,6 +9,7 @@ import com.dmh.msaccounts.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,14 +31,24 @@ public class TransactionController {
     ResponseHandler responseHandler;
 
     @PostMapping("/{id}/transactions")
-    public ResponseEntity<Object> transaction(@NotNull @RequestBody CardsTypeEnum cardType,
-                                              @PathVariable("id") String id,
-                                              @NotNull @RequestBody Double value,
-                                              @RequestBody String description){
+    public ResponseEntity<Object> transaction(@PathVariable("id") String id,
+                                              @Valid @RequestBody TransactionDtoRequest transactionDtoRequest,
+                                              BindingResult bindingResult) throws Exception {
+
+        if (bindingResult.hasErrors()){
+            throw new Exception(String.valueOf(bindingResult.getAllErrors().get(0)));
+        }
 
         String accountId = accountService.findAccountById(id).getAccount();
 
-        Transactions transaction = new Transactions(cardType, accountId, new BigDecimal(value), new Date(), "cash deposit", description);
+        Transactions transaction = new Transactions(
+                transactionDtoRequest.getCardType(),
+                accountId,
+                transactionDtoRequest.getValue(),
+                new Date(),
+                "cash deposit",
+                transactionDtoRequest.getDescription());
+
         return responseHandler.build(transactionService.transferirValor(transaction), HttpStatus.OK, "Successfully transferred");
     }
 }
--- ms-accounts/src/main/java/com/dmh/msaccounts/repository/IAccountsRepository.java
+++ ms-accounts/src/main/java/com/dmh/msaccounts/repository/IAccountsRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface IAccountsRepository extends MongoRepository<Accounts, String> {
-    Optional<AccountsDTORequest> findAccountCardsById(String accountId, String cardId);
+
 }
