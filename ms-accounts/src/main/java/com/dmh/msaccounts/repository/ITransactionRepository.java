2aebd90d59a280ce01bb2efdc566574220fd4880
Jardel Silva <jardeloliver197@gmail.com>
2023-03-21 11:09:38 -0300

    refatorando transactions

--- ms-accounts/src/main/java/com/dmh/msaccounts/repository/ITransactionRepository.java
+++ ms-accounts/src/main/java/com/dmh/msaccounts/repository/ITransactionRepository.java
@@ -2,9 +2,13 @@ package com.dmh.msaccounts.repository;
 
 import com.dmh.msaccounts.model.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ITransactionRepository extends JpaRepository<Transactions, Long> {
+
+    @Query("from Deposit")
     List<Transactions> findTop5ByAccountOriginIdOrderByDateTransactionDesc(String accountId);
+
 }
